7be6f31a11fe98d5741a4778e9d3698492edb8f5
Shreehar-KE <shreehar.elancheran@gmail.com>
2025-03-06 19:05:11 +0530

    fix output to include orignal traceback

--- pyiwy/handler.py
+++ pyiwy/handler.py
@@ -1,13 +1,16 @@
 # pyiwy/handler.py
 
 import sys
+import traceback as tb
 from .exceptions_data import get_funny_message
 
 
 def humor_excepthook(exc_type, exc_value, traceback):
     """Custom exception hook to print humorous messages instead of boring stack traces."""
     funny_message = get_funny_message(exc_type)
-    print(f"🔥 ERROR ALERT 🔥\n{funny_message}\n\nOriginal Error: {exc_value}")
+    for line in tb.format_tb(traceback):
+        print (line)
+    print(f"🔥 ERROR ALERT 🔥\n{funny_message}\n\nOriginal Error:\n {exc_type.__name__}: {exc_value}")
 
 
 # Set the custom exception handler globally
